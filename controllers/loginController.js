2009cdd5a890354373cf7d1390a1caab27937c40
OsmanSivrikaya <95168500+OsmanSivrikaya@users.noreply.github.com>
2024-02-12 13:49:26 +0000

    Final JWT Token Basic Example

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,12 +5,15 @@ const jwt = require("jsonwebtoken");
 exports.login = (req, res) => {
   const { username, password } = req.body;
   // Basit bir kullanıcı adı ve şifre kontrolü
-  if (username === "admin" && password === "password") {
-    // Kimlik doğrulaması başarılı olduğunda bir JWT oluştur
-    // Burada oluşan token süresi 1 saat dir.
-    const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
-    res.json({ token });
+  if (username === 'admin' && password === 'password') {
+      // Kullanıcı adı ve şifre doğruysa ve kullanıcı admin ise, role olarak 'admin' belirle
+      const token = jwt.sign({ username, role: 'admin' }, secretKey, { expiresIn: '1h' });
+      res.json({ token });
+  } else if (username === 'user' && password === 'password') {
+      // Kullanıcı adı ve şifre doğruysa ve kullanıcı normal bir kullanıcı ise, role olarak 'user' belirle
+      const token = jwt.sign({ username, role: 'user' }, secretKey, { expiresIn: '1h' });
+      res.json({ token });
   } else {
-    res.status(401).json({ message: "Invalid username or password" });
+      res.status(401).json({ message: 'Invalid username or password' });
   }
 };
